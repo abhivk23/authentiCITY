a68b6d80d0df46f6002f83f3285b99b9b77afa29
Irvin Ives Lau <irvin.lau.kto@axa.com.hk>
2020-12-17 17:27:26 +0800

    feat: query pins table api route

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,7 @@ app.listen(port, () => {
 /** Sqlite3 module configurations */
 const sqlite3 = require("sqlite3").verbose();
 
-const DBName = "hashtags";
+const DBName = "data";
 
 /** SQL Database connection */
 const db = new sqlite3.Database(`./${DBName}.db`, err => {
@@ -21,42 +21,10 @@ const db = new sqlite3.Database(`./${DBName}.db`, err => {
 });
 
 
-app.get("/coords", (req, res) => {
-  res.send([{
-    title: 'title1',
-    lat: 123,
-    lng: 123,
-  },
-  {
-    title: 'title1',
-    lat: 123,
-    lng: 123,
-  }])
-})
+app.get("/api/pins", (req, res) => {
+  const sqlSelect = `SELECT * FROM pins`;
 
-app.post("/generated", (req, res) => {
-  const mainCategories = req.body.mainCategories;
-  const subCategories = req.body.subCategories;
-  const captionStyle = req.body.document;
-
-  const sqlSelect = `SELECT ${subCategories} FROM ${mainCategories} ORDER BY RANDOM() LIMIT 30`;
-  /** Select all values from database using SQL statement initialized above */
-
-  db.all(sqlSelect, (err, generatedText) => {
-    let list = [];
-    /** Append each value to array */
-    generatedText.forEach(row => {
-      list.push(row[`${subCategories}`]);
-    });
-    /** Convert array to string */
-    let hashtags = String(list);
-    /** Replace comma ',' with new line '\n' */
-    hashtags = hashtags.replace(/,/g, "\n");
-    /** Declare data to be used/rendered */
-    const data = {
-      hashtags: hashtags,
-      captionStyle: captionStyle
-    };
-    res.render("generated", { locals: data });
+  db.all(sqlSelect, (err, data) => {
+    res.send(data);
   });
 });
